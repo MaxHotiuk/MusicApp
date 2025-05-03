f0e5601f349e43f9527de90457799095f6d9e227
bezshumu <103311303+MaxHotiuk@users.noreply.github.com>
2025-05-04 00:26:38 +0300

    feat: Add database migration on application startup

--- MusicApp/Program.cs
+++ MusicApp/Program.cs
@@ -64,6 +64,12 @@ builder.Services.Configure<FormOptions>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
